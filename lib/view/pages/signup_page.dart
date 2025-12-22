96ff415332c602e71f05e94df3a771e26f714b93
joshitha0818 <joshitha0818@gmail.com>
2025-12-22 23:50:13 +0530

    Add: implement SignupPage with custom fields and gradient background

---
+++ lib/view/pages/signup_page.dart
@@ -0,0 +1,44 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/view/widgets/auth_gradient_page.dart';
+import 'package:client/view/widgets/custom_field.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Pallete.backgroundColor),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Sign up.',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 30),
+            CustomField(hintText: 'Name'),
+            const SizedBox(height: 15),
+            CustomField(hintText: 'Email'),
+            const SizedBox(height: 15),
+            CustomField(hintText: 'Password'),
+            const SizedBox(height: 20),
+            AuthGradientPage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
