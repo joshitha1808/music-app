3acedad89fc724d9361133ab2b241dadcf651d40
joshitha0818 <joshitha0818@gmail.com>
2025-12-22 23:49:59 +0530

    Add: enhance dark theme with input decoration styles

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,7 +2,17 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+    borderSide: BorderSide(color: color, width: 3),
+
+    borderRadius: BorderRadius.circular(10),
+  );
   static final darkThemeMode = ThemeData().copyWith(
     scaffoldBackgroundColor: Pallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(27),
+      enabledBorder: _border(Pallete.borderColor),
+      focusedBorder: _border(Pallete.gradient2),
+    ),
   );
 }
