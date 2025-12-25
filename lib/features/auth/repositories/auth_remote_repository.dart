b419f70576fc255da027535ab3d6f35400d7902a
joshitha0818 <joshitha0818@gmail.com>
2025-12-26 01:02:28 +0530

    fix: handle exceptions using try-catch

--- lib/features/auth/repositories/auth_remote_repository.dart
+++ lib/features/auth/repositories/auth_remote_repository.dart
@@ -8,24 +8,32 @@ class AuthRemoteRepository {
     required String email,
     required String password,
   }) async {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2:8000/auth/signup'),
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/auth/signup'),
 
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'name': name, 'email': email, 'password': password}),
-    );
-    print(response.body);
-    print(response.statusCode);
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'name': name, 'email': email, 'password': password}),
+      );
+      print(response.body);
+      print(response.statusCode);
+    } catch (e) {
+      print(e);
+    }
   }
 
   Future<void> login({required String email, required String password}) async {
-    final response = await http.post(
-      Uri.parse('http://10.0.2.2:8000/auth/login'),
+    try {
+      final response = await http.post(
+        Uri.parse('http://10.0.2.2:8000/auth/login'),
 
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'email': email, 'password': password}),
-    );
-    print(response.body);
-    print(response.statusCode);
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': email, 'password': password}),
+      );
+      print(response.body);
+      print(response.statusCode);
+    } catch (e) {
+      print(e);
+    }
   }
 }
