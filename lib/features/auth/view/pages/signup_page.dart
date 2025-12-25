b7cdb7348b069766c56494a717b96a2d3f0f40f0
joshitha0818 <joshitha0818@gmail.com>
2025-12-26 00:07:49 +0530

    feat: call auth signup API from signup page

---
+++ lib/features/auth/view/pages/signup_page.dart
@@ -0,0 +1,91 @@
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/features/auth/repositories/auth_remote_repository.dart';
+import 'package:client/features/auth/view/widgets/auth_gradient_page.dart';
+import 'package:client/features/auth/view/widgets/custom_field.dart';
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Pallete.backgroundColor),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign up.',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 30),
+              CustomField(hintText: 'Name', controller: nameController),
+              const SizedBox(height: 15),
+              CustomField(hintText: 'Email', controller: emailController),
+              const SizedBox(height: 15),
+              CustomField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20),
+              AuthGradientPage(
+                buttonText: 'Sign uP',
+                onTap: () async {
+                  await AuthRemoteRepository().signup(
+                    name: nameController.text,
+                    email: emailController.text,
+                    password: passwordController.text,
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Already  have  an  acount?  ',
+                  style: TextStyle(
+                    fontSize: 14,
+                    //fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+
+                  children: const [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: TextStyle(color: Pallete.gradient2),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
