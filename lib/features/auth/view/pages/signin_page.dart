048bed3a321fca175cc42c194819e8d313f9ffbb
joshitha0818 <joshitha0818@gmail.com>
2025-12-27 00:53:00 +0530

    feat: add loading state and SnackBar handling in sign-in form

--- lib/features/auth/view/pages/signin_page.dart
+++ lib/features/auth/view/pages/signin_page.dart
@@ -1,8 +1,11 @@
 import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
+import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/auth/repositories/auth_remote_repository.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_page.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
+import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
@@ -28,83 +31,100 @@ class _SignupPageState extends ConsumerState<SigninPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authViewmodelProvider)?.isLoading == true;
+    ref.listen(authViewmodelProvider, (_, next) {
+      next?.when(
+        data: (data) {
+          showSnackBar(context, 'Account created sucessfully! please login');
+        },
+        error: (error, st) {
+          showSnackBar(context, error.toString());
+        },
+
+        loading: () {},
+      );
+    });
     return Scaffold(
       appBar: AppBar(backgroundColor: Pallete.backgroundColor),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Sign In.',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-
-              const SizedBox(height: 15),
-              CustomField(
-                hintText: 'Email',
-                controller: emailController,
-                autofocus: false,
-              ),
-              const SizedBox(height: 15),
-              CustomField(
-                hintText: 'Password',
-                controller: passwordController,
-                isObscureText: true,
-                autofocus: false,
-              ),
-              const SizedBox(height: 20),
-              AuthGradientPage(
-                buttonText: 'Sign in',
-                onTap: () async {
-                  final res = await AuthRemoteRepository().login(
-                    email: emailController.text,
-                    password: passwordController.text,
-                  );
-                  final val = switch (res) {
-                    Left(value: final l) => l,
-                    Right(value: final r) => r,
-                  };
-                  print(val);
-                },
-              ),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SignupPage()),
-                  );
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text:
-                        "Don't have  an  acount?  ", //or 'Don\'t have an account
-                    style: TextStyle(
-                      fontSize: 14,
-                      //fontWeight: FontWeight.w700,
-                      color: Colors.white,
+      body: isLoading
+          ? const Loader()
+          : Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sign In.',
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
 
-                    children: const [
-                      TextSpan(
-                        text: 'Sign Up',
-                        style: TextStyle(color: Pallete.gradient2),
+                    const SizedBox(height: 15),
+                    CustomField(
+                      hintText: 'Email',
+                      controller: emailController,
+                      autofocus: false,
+                    ),
+                    const SizedBox(height: 15),
+                    CustomField(
+                      hintText: 'Password',
+                      controller: passwordController,
+                      isObscureText: true,
+                      autofocus: false,
+                    ),
+                    const SizedBox(height: 20),
+                    AuthGradientPage(
+                      buttonText: 'Sign in',
+                      onTap: () async {
+                        final res = await AuthRemoteRepository().login(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+                        final val = switch (res) {
+                          Left(value: final l) => l,
+                          Right(value: final r) => r,
+                        };
+                        print(val);
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignupPage(),
+                          ),
+                        );
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          text:
+                              "Don't have  an  acount?  ", //or 'Don\'t have an account
+                          style: TextStyle(
+                            fontSize: 14,
+                            //fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+
+                          children: const [
+                            TextSpan(
+                              text: 'Sign Up',
+                              style: TextStyle(color: Pallete.gradient2),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
