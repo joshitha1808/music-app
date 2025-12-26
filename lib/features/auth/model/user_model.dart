7982e7e9216182fa4815ba07c6b7b3ce18a470de
joshitha0818 <joshitha0818@gmail.com>
2025-12-26 20:41:14 +0530

    refactor: adjust code formatting

--- lib/features/auth/model/user_model.dart
+++ lib/features/auth/model/user_model.dart
@@ -5,17 +5,9 @@ class UserModel {
   final String name;
   final String email;
   final String id;
-  UserModel({
-    required this.name,
-    required this.email,
-    required this.id,
-  });
+  UserModel({required this.name, required this.email, required this.id});
 
-  UserModel copyWith({
-    String? name,
-    String? email,
-    String? id,
-  }) {
+  UserModel copyWith({String? name, String? email, String? id}) {
     return UserModel(
       name: name ?? this.name,
       email: email ?? this.email,
@@ -24,11 +16,7 @@ class UserModel {
   }
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'name': name,
-      'email': email,
-      'id': id,
-    };
+    return <String, dynamic>{'name': name, 'email': email, 'id': id};
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
@@ -41,7 +29,8 @@ class UserModel {
 
   String toJson() => json.encode(toMap());
 
-  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() => 'UserModel(name: $name, email: $email, id: $id)';
@@ -49,11 +38,8 @@ class UserModel {
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.name == name &&
-      other.email == email &&
-      other.id == id;
+
+    return other.name == name && other.email == email && other.id == id;
   }
 
   @override
