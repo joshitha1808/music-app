5b37e401477e3234b9ef09293ec64161ad9f5bc2
joshitha1808 <joshitha0818@gmail.com>
2025-12-28 23:13:49 +0530

    feat: connect AuthViewModel using ProviderScope parent

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:client/core/theme/theme.dart';
+import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main()async {
   WidgetsFlutterBinding.ensureInitialized();
+  final container=ProviderContainer();
+  await container.read(authViewModelProvider.notifier).initSharedPreferences();
+  
   runApp(
-    ProviderScope(child: const MyApp()
+    ProviderScope(
+      parent: container,
+      child: const MyApp()
     ),
   );
 }
