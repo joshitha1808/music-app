837a71aa4362a409f2f542734115f0fa2585f380
joshitha1808 <joshitha0818@gmail.com>
2025-12-28 22:54:13 +0530

    fix: initialize Flutter bindings before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     ProviderScope(child: const MyApp()
     ),
