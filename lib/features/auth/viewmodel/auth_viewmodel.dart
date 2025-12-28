a67e3a1ad7fdcf84803f30307ee266ddda3362c2
Joshitha <joshitha0818@example.com>
2025-12-28 21:41:52 +0530

    feat: add AuthLocalRepository

--- lib/features/auth/viewmodel/auth_viewmodel.dart
+++ lib/features/auth/viewmodel/auth_viewmodel.dart
@@ -1,59 +1,56 @@
-import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:fpdart/fpdart.dart';
 
 import 'package:client/features/auth/model/user_model.dart';
 import 'package:client/features/auth/repositories/auth_remote_repository.dart';
+import 'package:client/features/auth/repositories/auth_local_repository.dart';
 
 part 'auth_viewmodel.g.dart';
 
 @riverpod
-class AuthViewmodel extends _$AuthViewmodel {
+class AuthViewModel extends _$AuthViewModel {
   late AuthRemoteRepository _authRemoteRepository;
+  late AuthLocalRepository _authLocalRepository;
+
 
   @override
   AsyncValue<UserModel>? build() {
-    _authRemoteRepository=ref.watch(provider)
+    _authRemoteRepository = ref.watch(authRemoteRepositoryProvider);
+    _authLocalRepository=ref.watch(authLocalRepositoryProvider);
+  
     return null;
   }
 
+  Future<void> initSharedPreferences() async{
+    await _authLocalRepository.init();
+
+  }
+
+ 
+
+  /// SIGN UP
   Future<void> signUpUser({
     required String name,
     required String email,
     required String password,
   }) async {
     state = const AsyncValue.loading();
+
     final res = await _authRemoteRepository.signup(
       name: name,
       email: email,
       password: password,
     );
-    //pattern matching
-    final val = switch (res) {
-      Left(value: final l) => state = AsyncValue.error(
-        l.message,
-        StackTrace.current,
-      ),
-      Right(value: final r) => state = AsyncValue.data(r), //sucess return name
+
+    final val=switch(res){
+      Left(value:final l)=>state= AsyncValue.error(l.message, StackTrace.current,),
+      Right(value:final r)=>state=AsyncValue.data(r),
     };
     print(val);
   }
 
-  Future<void> signinUser({
-    required String email,
-    required String password,
-  }) async {
-    state = const AsyncValue.loading();
-    final res = await _authRemoteRepository.login(
-      email: email,
-      password: password,
-    );
-    //pattern matching
-    final val = switch (res) {
-      Left(value: final l) => state = AsyncValue.error(
-        l.message,
-        StackTrace.current,
-      ),
-      Right(value: final r) => state = AsyncValue.data(r), //sucess return name
-    };
+  AsyncValue<UserModel>? _loginSuccess(UserModel user){
+    _authLocalRepository.setToken(user.token);
+    return state=AsyncValue.data(user);
   }
 }
