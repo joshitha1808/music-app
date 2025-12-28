0b2cc0c65c171ad77c1ab6842d6e4bb49b79c21c
Joshitha <joshitha0818@example.com>
2025-12-28 21:38:06 +0530

    chore: remove auth remote repository files

--- lib/features/auth/repositories/auth_remote_repository.g.dart
+++ lib/features/auth/repositories/auth_remote_repository.g.dart
@@ -6,53 +6,24 @@ part of 'auth_remote_repository.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, type=warning
-
-@ProviderFor(authRemoteRepository)
-const authRemoteRepositoryProvider = AuthRemoteRepositoryProvider._();
-
-final class AuthRemoteRepositoryProvider
-    extends
-        $FunctionalProvider<
-          AuthRemoteRepository,
-          AuthRemoteRepository,
-          AuthRemoteRepository
-        >
-    with $Provider<AuthRemoteRepository> {
-  const AuthRemoteRepositoryProvider._()
-    : super(
-        from: null,
-        argument: null,
-        retry: null,
-        name: r'authRemoteRepositoryProvider',
-        isAutoDispose: true,
-        dependencies: null,
-        $allTransitiveDependencies: null,
-      );
-
-  @override
-  String debugGetCreateSourceHash() => _$authRemoteRepositoryHash();
-
-  @$internal
-  @override
-  $ProviderElement<AuthRemoteRepository> $createElement(
-    $ProviderPointer pointer,
-  ) => $ProviderElement(pointer);
-
-  @override
-  AuthRemoteRepository create(Ref ref) {
-    return authRemoteRepository(ref);
-  }
-
-  /// {@macro riverpod.override_with_value}
-  Override overrideWithValue(AuthRemoteRepository value) {
-    return $ProviderOverride(
-      origin: this,
-      providerOverride: $SyncValueProvider<AuthRemoteRepository>(value),
-    );
-  }
-}
-
 String _$authRemoteRepositoryHash() =>
     r'd4d3598d49a62c6eddebf6bb77f2fa5eeca4809c';
+
+/// See also [authRemoteRepository].
+@ProviderFor(authRemoteRepository)
+final authRemoteRepositoryProvider =
+    AutoDisposeProvider<AuthRemoteRepository>.internal(
+      authRemoteRepository,
+      name: r'authRemoteRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$authRemoteRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef AuthRemoteRepositoryRef = AutoDisposeProviderRef<AuthRemoteRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
