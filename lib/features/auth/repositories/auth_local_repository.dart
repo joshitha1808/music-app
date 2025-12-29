9cf347514740b9eee21b295e19e1f7cd51cdd4f4
joshitha1808 <joshitha0818@gmail.com>
2025-12-29 17:28:14 +0530

    feat: enable keepAlive

--- lib/features/auth/repositories/auth_local_repository.dart
+++ lib/features/auth/repositories/auth_local_repository.dart
@@ -3,25 +3,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 part 'auth_local_repository.g.dart';
 
-@riverpod
-AuthLocalRepository authLocalRepository(AuthLocalRepositoryRef ref){
+@Riverpod(keepAlive: true)
+AuthLocalRepository authLocalRepository(AuthLocalRepositoryRef ref) {
   return AuthLocalRepository();
 }
 
 class AuthLocalRepository {
   late SharedPreferences _sharedPreferences;
 
-  Future<void> init() async{
-    _sharedPreferences =await SharedPreferences.getInstance();
+  Future<void> init() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
   }
 
-  void setToken(String? token){
-    if(token!=null){
+  void setToken(String? token) {
+    if (token != null) {
       _sharedPreferences.setString('x-auth-token', token);
     }
   }
 
-  String? getToken(){
+  String? getToken() {
     return _sharedPreferences.getString('x-auth-token');
   }
 }
