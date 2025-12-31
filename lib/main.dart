d67426395c1cb4f8a6b671a6f06a9f7825b8a81e
joshitha1808 <joshitha0818@gmail.com>
2025-12-31 23:44:25 +0530

    style: remove extra spaces

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ void main() async {
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentUser = ref.watch(currentUserNotifierProvider);
