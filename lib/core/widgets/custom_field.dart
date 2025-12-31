12bcf3fb59a100db9792ca7ce8587591c9cacd08
joshitha1808 <joshitha0818@gmail.com>
2025-12-31 14:46:55 +0530

    feat: disable readonly mode

---
+++ lib/core/widgets/custom_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomField extends StatelessWidget {
+  final bool isObscureText;
+  final String hintText;
+  final TextEditingController? controller;
+  final bool readOnly;
+  const CustomField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+    required bool autofocus,
+    this.readOnly = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      readOnly: readOnly,
+      decoration: InputDecoration(hintText: hintText),
+      controller: controller,
+      validator: (val) {
+        if (val!.trim().isEmpty) {
+          return "$hintText is missing!";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
+      style: TextStyle(color: Colors.white),
+    );
+  }
+}
