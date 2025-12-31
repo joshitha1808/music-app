d0ad0656c9ff9461933c172cf9ab1639d284a458
joshitha1808 <joshitha0818@gmail.com>
2025-12-31 23:44:00 +0530

    feat: add audio file picker function

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
 
 void showSnackBar(BuildContext context, String message) {
   final messenger = ScaffoldMessenger.of(context);
 
   messenger.hideCurrentSnackBar();
   messenger.showSnackBar(SnackBar(content: Text(message)));
+}
+
+Future<File?> pickAudioFile() async {
+  try {
+    final filePickerRes = await FilePicker.platform.pickFiles(
+      type: FileType.audio,
+    );
+
+    if (filePickerRes != null) {
+      String? filePath = filePickerRes!.files.first.xFile.path;
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
 }
