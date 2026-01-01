9968fe3a952ec81c5e795dccbb8b37bd21ee4503
joshitha1808 <joshitha0818@gmail.com>
2026-01-01 14:31:52 +0530

    feat: create path variable for audio wave path.

---
+++ lib/features/home/view/widgets/audio_wave.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AudioWave extends StatefulWidget {
+  final String path;
+  const AudioWave({super.key, required this.path});
+
+  @override
+  State<AudioWave> createState() => _AudioWaveState();
+}
+
+class _AudioWaveState extends State<AudioWave> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
