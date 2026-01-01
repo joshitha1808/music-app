33fcd09547927e6d20a777dce2cd0420ce4af704
joshitha1808 <joshitha0818@gmail.com>
2026-01-01 14:34:22 +0530

    feat: initialize PlayerController in AudioWave widget

--- lib/features/home/view/widgets/audio_wave.dart
+++ lib/features/home/view/widgets/audio_wave.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audio_waveforms/audio_waveforms.dart';
 
 class AudioWave extends StatefulWidget {
   final String path;
@@ -9,6 +10,7 @@ class AudioWave extends StatefulWidget {
 }
 
 class _AudioWaveState extends State<AudioWave> {
+  final PlayerController playerController = PlayerController();
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
