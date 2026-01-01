1d8d3eca186db4369497deada2994b56d74a33f2
joshitha1808 <joshitha0818@gmail.com>
2026-01-01 23:10:25 +0530

    fix: update serverURL for Android to use correct IP address

--- lib/core/server_constants.dart
+++ lib/core/server_constants.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
-class ServerConstant{
-  static String serverURL= Platform.isAndroid ?'http://10.0.2.2:8000' :'http://127.0.0.1:8000';
-
+class ServerConstant {
+  static String serverURL = Platform.isAndroid
+      ? 'http://192.168.0.3:8000'
+      : 'http://127.0.0.1:8000';
 }
