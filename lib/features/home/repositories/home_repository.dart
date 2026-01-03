c5350002132abce7b7364479bb26e730347024bb
joshitha1808 <joshitha0818@gmail.com>
2026-01-03 23:04:17 +0530

    style: format uploadSong method for improved readability

--- lib/features/home/repositories/home_repository.dart
+++ lib/features/home/repositories/home_repository.dart
@@ -1,17 +1,24 @@
+import 'dart:io';
+
 import 'Package:http/http.dart' as http;
 import 'package:client/core/server_constants.dart';
 
 class HomeRepository {
-  Future<void> uploadSong(File selectedImage,
-  File selectedAudio) async {
+  Future<void> uploadSong(File selectedImage, File selectedAudio) async {
     final request = http.MultipartRequest(
       'POST',
       Uri.parse('${ServerConstant.serverURL}/song/upload'),
     );
     request
       ..files.addAll([
-        await http.MultipartFile.fromPath('song', '/https://prosearch.tribeofnoise.com/artists/show/84070/43981/'),
-        await http.MultipartFile.fromPath('thumbnail', 'https://prosearch.tribeofnoise.com/artists/show/24142/43452/'),
+        await http.MultipartFile.fromPath(
+          'song',
+          '/https://prosearch.tribeofnoise.com/artists/show/84070/43981/',
+        ),
+        await http.MultipartFile.fromPath(
+          'thumbnail',
+          'https://prosearch.tribeofnoise.com/artists/show/24142/43452/',
+        ),
       ])
       ..fields.addAll({
         'artist': 'Taylor',
@@ -22,7 +29,7 @@ class HomeRepository {
         'x-auth-token':
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRiMjgzYjMyLWVjNWEtNGYxNy1iMmRlLTRkZDBmYjgwNjFhOSJ9.qIusTp49iEiX0z_1uGwYFjIAt7vAX-7KKKkRthRWyrg',
       });
-      final res=await request.send();
-      print(res);
+    final res = await request.send();
+    print(res);
   }
 }
