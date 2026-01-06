fd9e42f30a75795c84237fd86dd9933e52b55806
joshitha1808 <joshitha0818@gmail.com>
2026-01-07 02:04:03 +0530

    feat: connect HomeViewmodel to HomeRepository

--- lib/features/home/viewmodel/home_viewmodel.dart
+++ lib/features/home/viewmodel/home_viewmodel.dart
@@ -1,14 +1,17 @@
 import 'dart:io';
 import 'dart:ui';
 
+import 'package:client/features/home/repositories/home_repository.dart';
 import 'package:client/features/home/view/pages/upload_songs_page.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'home_viewmodel.g.dart';
 
 @riverpod
 class HomeViewmodel extends _$HomeViewmodel {
+  late HomeRepository _homeRepository;
   @override
   AsyncValue? build() {
+    _homeRepository=ref.watch(homeRepositoryProvider);
     return null;
   }
 
