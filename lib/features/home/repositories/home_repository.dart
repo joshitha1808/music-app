8f98460f8a0e5c67f18dd81367878faed1ed623d
joshitha1808 <joshitha0818@gmail.com>
2026-01-07 01:25:43 +0530

    refactor: return Either using fpdart and handle response with try-catch.

--- lib/features/home/repositories/home_repository.dart
+++ lib/features/home/repositories/home_repository.dart
@@ -2,31 +2,37 @@ import 'dart:io';
 
 import 'Package:http/http.dart' as http;
 import 'package:client/core/server_constants.dart';
+import 'package:client/core/theme/failure/failure.dart';
+import 'package:fpdart/fpdart.dart';
 
 class HomeRepository {
-  Future<void> uploadSong(File selectedImage, File selectedAudio) async {
-    final request = http.MultipartRequest(
-      'POST',
-      Uri.parse('${ServerConstant.serverURL}/song/upload'),
-    );
-    request
-      ..files.addAll([
-        await http.MultipartFile.fromPath('song', selectedAudio.path),
-        await http.MultipartFile.fromPath(
-          'thumbnail',selectedImage.path
-,
-        ),
-      ])
-      ..fields.addAll({
-        'artist': 'Taylor',
-        'song_name': 'Whoosh',
-        'hex_code': 'FFFFFF',
-      })
-      ..headers.addAll({
-        'x-auth-token':
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRiMjgzYjMyLWVjNWEtNGYxNy1iMmRlLTRkZDBmYjgwNjFhOSJ9.qIusTp49iEiX0z_1uGwYFjIAt7vAX-7KKKkRthRWyrg',
-      });
-    final res = await request.send();
-    print(res);
+  Future<Either<AppFailure, String>> uploadSong(
+    File selectedImage,
+    File selectedAudio,
+  ) async {
+    try {
+      final request = http.MultipartRequest(
+        'POST',
+        Uri.parse('${ServerConstant.serverURL}/song/upload'),
+      );
+      request
+        ..files.addAll([
+          await http.MultipartFile.fromPath('song', selectedAudio.path),
+          await http.MultipartFile.fromPath('thumbnail', selectedImage.path),
+        ])
+        ..fields.addAll({
+          'artist': 'Taylor',
+          'song_name': 'Whoosh',
+          'hex_code': 'FFFFFF',
+        })
+        ..headers.addAll({
+          'x-auth-token':
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRiMjgzYjMyLWVjNWEtNGYxNy1iMmRlLTRkZDBmYjgwNjFhOSJ9.qIusTp49iEiX0z_1uGwYFjIAt7vAX-7KKKkRthRWyrg',
+        });
+      final res = await request.send();
+      return Right(await res.stream.bytesToString());
+    } catch (e) {
+      return Left(AppFailure(e.toString()));
+    }
   }
 }
