ce3a4e3532bc1c1c25ac2b87f5cf45cdd95b025f
joshitha1808 <joshitha0818@gmail.com>
2026-01-07 01:38:44 +0530

    fix: add build method to HomeViewmodel for riverpod generator.

---
+++ lib/features/home/viewmodel/home_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'home_viewmodel.g.dart';
+
+@riverpod
+class HomeViewmodel extends _$HomeViewmodel {
+  @override
+  AsyncValue? build() {
+    return null;
+  }
+}
