c6f1ed3084cd54a79b12b57368636d5749ecf69a
joshitha1808 <joshitha0818@gmail.com>
2026-01-07 00:16:54 +0530

    fix: update Android IPv4 address.

--- lib/core/server_constants.dart
+++ lib/core/server_constants.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 
 class ServerConstant {
   static String serverURL = Platform.isAndroid
-      ? 'http://172.18.144.57:8000'
+      ? 'http://192.168.137.211:8000'
       : 'http://127.0.0.1:8000';
 }
