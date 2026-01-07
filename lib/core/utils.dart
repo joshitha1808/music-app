4c6d049e4db4de02c693119d9a0a73980040d0e7
joshitha1808 <joshitha0818@gmail.com>
2026-01-07 12:49:59 +0530

    feat: convert hexcode to color.

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -5,10 +5,9 @@ import 'dart:io';
 String rgbToHex(Color color){ 
   return '${color.red.toRadixString(16).padLeft(2,'0')}${color.blue.toRadixString(16).padLeft(2,'0')}${color.green.toRadixString(16).padLeft(2,'0')}'
 }
-
- 
-
-
+Color hexToColor(String hex){
+  return Color(int.parse(hex,radix: 16)+0xFF000000);
+}
 void showSnackBar(BuildContext context, String message) {
   final messenger = ScaffoldMessenger.of(context);
 
