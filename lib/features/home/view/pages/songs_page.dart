5520a36a9a81008f99ae957923e9b6e131ac5f9d
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 00:57:06 +0530

    feat: handle getAllSongsProvider states

--- lib/features/home/view/pages/songs_page.dart
+++ lib/features/home/view/pages/songs_page.dart
@@ -1,3 +1,5 @@
+import 'package:client/core/widgets/loader.dart';
+import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -5,14 +7,30 @@ class SongsPage extends ConsumerWidget {
   const SongsPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef Ref) {
-    return Column(
-      children: [
-        Text(
-          'Latest today',
-          style: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
-        ),
-      ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Latest today',
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
+            ),
+          ),
+          ref
+              .watch(getAllSongsProvider)
+              .when(
+                data: (songs) {
+                  return Text('Hello');
+                },
+                error: (error, st) {
+                  return Center(child: Text(error.toString()));
+                },
+                loading: () => const Loader(),
+              ),
+        ],
+      ),
     );
   }
 }
