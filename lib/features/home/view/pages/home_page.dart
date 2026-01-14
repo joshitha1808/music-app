191a7683f41fc31db8e0b6ac4466cd9304b0ad85
joshitha1808 <joshitha0818@gmail.com>
2026-01-14 22:28:40 +0530

    fix: convert consumer widget to consumer stateful widget.

--- lib/features/home/view/pages/home_page.dart
+++ lib/features/home/view/pages/home_page.dart
@@ -2,11 +2,16 @@ import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
+  Widget build(BuildContext context) {
     final user = ref.watch(currentUserNotifierProvider);
     print(user);
     return Scaffold(
