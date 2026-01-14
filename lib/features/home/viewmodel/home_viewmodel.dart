a9e1ebe1a519b136abeaf22579ff6845735f7376
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 00:04:04 +0530

    feat: fetch songs using auth token and handle Either result

--- lib/features/home/viewmodel/home_viewmodel.dart
+++ lib/features/home/viewmodel/home_viewmodel.dart
@@ -3,11 +3,23 @@ import 'dart:ui';
 
 import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:client/core/utils.dart';
+import 'package:client/features/home/models/song_model.dart';
 import 'package:client/features/home/repositories/home_repository.dart';
 import 'package:flutter/rendering.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'home_viewmodel.g.dart';
+
+@riverpod
+Future<List<SongModel>> getAllSongs(GetAllSongsRef ref) async {
+  final token = ref.watch(currentUserNotifierProvider)!.token;
+  final res = await ref.watch(homeRepositoryProvider).getAllSongs(token: token);
+
+  return switch (res) {
+    Left(value: final l) => throw l.message,
+    Right(value: final r) => r,
+  };
+}
 
 @riverpod
 class HomeViewmodel extends _$HomeViewmodel {
