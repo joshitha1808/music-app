31b1aa5e2a231735bc0db984fb45f01e35c5ec12
joshitha1808 <joshitha0818@gmail.com>
2026-01-16 00:31:56 +0530

    feat: wrap container with hero

--- lib/features/home/view/widgets/music_player.dart
+++ lib/features/home/view/widgets/music_player.dart
@@ -49,13 +49,16 @@ class MusicPlayer extends ConsumerWidget {
               flex: 5,
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 30),
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(CurrentSong!.thumbnail_url),
-                      fit: BoxFit.cover,
+                child: Hero(
+                  tag: 'music-image',
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(CurrentSong!.thumbnail_url),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
               ),
