8fd4ea2ff2a990659974300c301ba385276f9f87
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 17:57:48 +0530

    style: wrap Column with Stack widget.

--- lib/features/home/view/widgets/music_slab.dart
+++ lib/features/home/view/widgets/music_slab.dart
@@ -16,63 +16,70 @@ class MusicSlab extends ConsumerWidget {
       return const SizedBox();
     }
 
-    return Container(
-      height: 66,
-      width: MediaQuery.of(context).size.width - 16,
-      decoration: BoxDecoration(
-        color: hexToColor(CurrentSong.hex_code),
-        borderRadius: BorderRadius.circular(9),
-      ),
-      padding: const EdgeInsets.all(9),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: 48,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(CurrentSong!.thumbnail_url),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(9),
-            ),
+    return Stack(
+      children: [
+        Container(
+          height: 66,
+          width: MediaQuery.of(context).size.width - 16,
+          decoration: BoxDecoration(
+            color: hexToColor(CurrentSong.hex_code),
+            borderRadius: BorderRadius.circular(9),
           ),
-          const SizedBox(width: 8),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
+          padding: const EdgeInsets.all(9),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                CurrentSong.song_name,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              ),
-              Text(
-                CurrentSong.artist,
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: Pallete.subtitleText,
+              Container(
+                width: 48,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(CurrentSong!.thumbnail_url),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(9),
                 ),
               ),
-              Row(
+              const SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(CupertinoIcons.heart, color: Pallete.whiteColor),
+                  Text(
+                    CurrentSong.song_name,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      CupertinoIcons.play_fill,
-                      color: Pallete.whiteColor,
+                  Text(
+                    CurrentSong.artist,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Pallete.subtitleText,
                     ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          CupertinoIcons.heart,
+                          color: Pallete.whiteColor,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          CupertinoIcons.play_fill,
+                          color: Pallete.whiteColor,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
