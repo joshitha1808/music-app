1d6ef4e3e2fc04f8ecb35814120392c070ee14d3
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 16:08:44 +0530

    feat: add current song notifier for song page

---
+++ lib/core/providers/current_song_notifier.dart
@@ -0,0 +1,12 @@
+import 'package:client/features/home/models/song_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'current_song_notifier.g.dart';
+
+@riverpod
+class CurrentSongNotifier extends _$CurrentSongNotifier {
+  @override
+  SongModel? build() {
+    return null;
+  }
+}
