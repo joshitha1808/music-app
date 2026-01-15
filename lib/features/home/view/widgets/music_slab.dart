86029fb1b8e14f4287cf1fbd0ba3297c9c3a39f1
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 16:38:34 +0530

    feat: add MusicSlab widget with current song listener

--- lib/features/home/view/widgets/music_slab.dart
+++ lib/features/home/view/widgets/music_slab.dart
@@ -1,3 +1,4 @@
+import 'package:client/core/providers/current_song_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +7,12 @@ class MusicSlab extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final CurrentSong = ref.watch(currentSongNotifierProvider);
+
+    if (CurrentSong == null) {
+      return const SizedBox();
+    }
+
     return Container();
   }
 }
