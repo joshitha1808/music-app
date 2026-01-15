77f9bf3b2f96ae6fd27e9f08a675c92270e3b50f
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 16:18:23 +0530

    feat: add update song method.

--- lib/core/providers/current_song_notifier.dart
+++ lib/core/providers/current_song_notifier.dart
@@ -1,12 +1,20 @@
 import 'package:client/features/home/models/song_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:just_audio/just_audio.dart';
 
 part 'current_song_notifier.g.dart';
 
 @riverpod
 class CurrentSongNotifier extends _$CurrentSongNotifier {
+  AudioPlayer? audioPlayer;
   @override
   SongModel? build() {
     return null;
+  }
+
+  void updateSong(SongModel song) {
+    audioPlayer = AudioPlayer();
+
+    final audioSource = AudioSource.uri(Uri.parse(song.song_url));
   }
 }
