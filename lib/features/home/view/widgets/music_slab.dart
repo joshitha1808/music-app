0d8cc3c6653ed06d620065744f2c28419a0252bb
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 16:27:37 +0530

    feat: add music slab consumer page

---
+++ lib/features/home/view/widgets/music_slab.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MusicSlab extends ConsumerWidget {
+  const MusicSlab({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
