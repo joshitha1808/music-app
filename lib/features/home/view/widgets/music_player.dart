f8c38210f255a6ed91fdc2833306c5e5d0a9ff93
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 21:46:59 +0530

    feat: add boxfit

--- lib/features/home/view/widgets/music_player.dart
+++ lib/features/home/view/widgets/music_player.dart
@@ -1,3 +1,5 @@
+import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/features/home/models/song_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +8,20 @@ class MusicPlayer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final CurrentSong = ref.watch(currentSongNotifierProvider);
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(CurrentSong!.thumbnail_url),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
