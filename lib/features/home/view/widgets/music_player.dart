34b6f5c0ea34a27ed73353394a54796a0cb53929
joshitha1808 <joshitha0818@gmail.com>
2026-01-15 22:14:40 +0530

    feat: add song title, artist, and favorite icon layout

--- lib/features/home/view/widgets/music_player.dart
+++ lib/features/home/view/widgets/music_player.dart
@@ -1,5 +1,7 @@
 import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/features/home/models/song_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -21,6 +23,42 @@ class MusicPlayer extends ConsumerWidget {
                   fit: BoxFit.cover,
                 ),
               ),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          CurrentSong.song_name,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: Pallete.whiteColor,
+                          ),
+                        ),
+                        Text(
+                          CurrentSong.artist,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Pallete.subtitleText,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Expanded(child: const SizedBox()),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(CupertinoIcons.heart),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
