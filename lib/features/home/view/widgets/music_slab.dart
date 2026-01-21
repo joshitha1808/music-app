a472ff897822a08113cb3cc77743d2e38faa8ca0
joshitha1808 <joshitha0818@gmail.com>
2026-01-21 23:52:26 +0530

    feat: Watch user favorites from provider

--- lib/features/home/view/widgets/music_slab.dart
+++ lib/features/home/view/widgets/music_slab.dart
@@ -1,4 +1,5 @@
 import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/core/providers/current_user_notifier.dart';
 import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/core/utils.dart';
 import 'package:client/features/home/view/widgets/music_player.dart';
@@ -13,6 +14,9 @@ class MusicSlab extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final currentSong = ref.watch(currentSongNotifierProvider);
     final songNotifier = ref.read(currentSongNotifierProvider.notifier);
+    final userFavorites = ref.watch(
+      currentUserNotifierProvider.select((data) => data!.favorites),
+    );
 
     if (currentSong == null) {
       return const SizedBox();
@@ -98,7 +102,14 @@ class MusicSlab extends ConsumerWidget {
 
                   IconButton(
                     onPressed: () {},
-                    icon: const Icon(CupertinoIcons.heart),
+                    icon: Icon(
+                      userFavorites
+                              .where((fav) => fav.song_id == currentSong.id)
+                              .toList()
+                              .isNotEmpty
+                          ? CupertinoIcons.heart_fill
+                          : CupertinoIcons.heart,
+                    ),
                     color: Colors.red,
                   ),
 
