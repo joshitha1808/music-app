fc5fad03e2fb9ab9dc5d282e3dd91d6e6695ee1b
joshitha1808 <joshitha0818@gmail.com>
2026-01-21 23:44:05 +0530

    feat: Add song metadata

--- lib/core/providers/current_song_notifier.dart
+++ lib/core/providers/current_song_notifier.dart
@@ -1,8 +1,8 @@
 import 'package:client/features/home/models/song_model.dart';
 import 'package:client/features/home/repositories/home_local_repository.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:just_audio/just_audio.dart';
-
 part 'current_song_notifier.g.dart';
 
 @riverpod
@@ -10,6 +10,7 @@ class CurrentSongNotifier extends _$CurrentSongNotifier {
   late HomeLocalRepository _homeLocalRepository;
   AudioPlayer? audioPlayer;
   bool isPlaying = false;
+
   @override
   SongModel? build() {
     _homeLocalRepository = ref.watch(homeLocalRepositoryProvider);
@@ -20,13 +21,23 @@ class CurrentSongNotifier extends _$CurrentSongNotifier {
     await audioPlayer?.stop();
     audioPlayer = AudioPlayer();
 
-    final audioSource = AudioSource.uri(Uri.parse(song.song_url));
+    final audioSource = AudioSource.uri(
+      Uri.parse(song.song_url),
+      tag: MediaItem(
+        id: song.id,
+        title: song.song_name,
+        artist: song.artist,
+        artUri: Uri.parse(song.thumbnail_url),
+      ),
+    );
     await audioPlayer!.setAudioSource(audioSource);
+
     audioPlayer!.playerStateStream.listen((state) {
       if (state.processingState == ProcessingState.completed) {
         audioPlayer!.seek(Duration.zero);
         audioPlayer!.pause();
         isPlaying = false;
+
         this.state = this.state?.copyWith(hex_code: this.state?.hex_code);
       }
     });
